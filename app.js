7474c15342e3cc62fa22a0a14a3b3b5f32c19807
Tri_Vo <vocaotri789@gmail.com>
2019-10-08 15:14:51 +0700

    make it better

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ cron.schedule('0 0 */23 * * *', () => {
         await page.type('input', '7411319985620');
         await page.click('#passwordNext');
         await page.waitFor(15000);
-        await page.click('#submitChallenge');
+        await page.$eval('input[type=submit]', el => el.click());
         await page.waitFor(7000);
         await browser.close();
         await console.log('Xong sau 23 h');
     })();
 });
 (async() => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
@@ -32,11 +32,11 @@ cron.schedule('0 0 */23 * * *', () => {
     await page.type('#identifierId', 'vocaotri445@gmail.com');
     await page.waitFor(1000);
     await page.click('#identifierNext');
-    await page.waitFor(5000);
+    await page.waitFor(2500);
     await page.type('input', '7411319985620');
     await page.click('#passwordNext');
     await page.waitFor(15000);
-    await page.click('#submitChallenge');
+    await page.$eval('input[type=submit]', el => el.click());
     await page.waitFor(7000);
     await browser.close();
     await console.log('Xong');
