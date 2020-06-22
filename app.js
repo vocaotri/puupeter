41e0552ce5ad163a37a3468b9bafa134a4905f74
Tri Vo <vocaotri445@gmail.com>
2020-06-22 17:22:06 +0700

    update code

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 var cron = require('node-cron');
 var express = require('express');
 var fs = require('fs').promises;
+var { performance } = require('perf_hooks');
 const app = express();
 const puppeteer = require('puppeteer');
 const port = process.env.PORT || 8080;
 var data= "";
 (async () => {
-    data = await fs.readFile('data2.csv');
+    data = await fs.readFile('data.csv');
     data = data.toString().split('\n');
+    var lenghtData = data.length - 1;
     var newdata = data.splice(1,data.length);
     newdata.map((el,index)=>{
         setTimeout(() => {
         var dataSecond = el.split(',');
             (async () => {
+            const t0 = performance.now();
             const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
             const page = await browser.newPage();
             await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
@@ -22,26 +25,28 @@ var data= "";
             await page.type('#entry_kana_kana01', dataSecond[3]);
             await page.type('#entry_kana_kana02', dataSecond[4]);
             await page.type('#entry_postal_code', dataSecond[6]+dataSecond[7]);
-            await page.waitFor(2000);
-            await page.evaluate( () => document.getElementById("entry_address_addr01").value = "")
+            await page.waitFor(1000);
+            await page.evaluate( () => document.getElementById("entry_address_addr01").value = "");
+            await page.evaluate( () => document.getElementById("entry_point").value = "");
             await page.type('#entry_address_addr01', dataSecond[9]);
-            await page.type('#entry_address_addr02', (dataSecond[10] != '')?dataSecond[10]:dataSecond[9]);
-            await page.type('#entry_phone_number', (dataSecond[12]+dataSecond[13]+dataSecond[14]+dataSecond[15]).replace(/\r/g, ""));
+            await page.type('#entry_address_addr02', dataSecond[10]);
+            await page.type('#entry_phone_number', (dataSecond[12]+dataSecond[13]+dataSecond[14]).replace(/\r/g, ""));
             await page.type('#entry_email_first', dataSecond[11]);
             await page.type('#entry_email_second', dataSecond[11]);
             await page.type('#entry_password_first', dataSecond[5]);
             await page.type('#entry_password_second', dataSecond[5]);
             await page.click('#entry_user_policy_check');
-            await page.waitFor(1000);
+            await page.type('#entry_point',dataSecond[15]);
             await page.keyboard.press('Enter');
-            await page.waitForNavigation();
             await page.waitFor(2000);
             await page.click('button[name="mode"]');
-            await page.waitFor(1000);
-            console.log('đã đăng ký');
             await browser.close();
+            const t1 = performance.now();
+            console.log('đã đăng ký xong user '+ (parseInt (index) + 1));
+            console.log("Thực hiện sau " + (t1 - t0)/1000 + " giây.");
+            if(parseInt(index) + 1 === lenghtData) console.log('Chúc mừng bạn đã xong ');
         })();
-    }, 17000 * index);
+    }, 13000 * index);
     });
 })();
 
